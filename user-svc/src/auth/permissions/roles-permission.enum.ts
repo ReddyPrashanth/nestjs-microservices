6991a9d494ba1c55c243482e8a8795ac475b1f22
ReddyPrashanth <s.prasantreddy94@gmail.com>
2021-12-07 22:46:55 -0600

    moved auth module to api gateway

--- user-svc/src/auth/permissions/roles-permission.enum.ts
+++
@@ -1,6 +0,0 @@
-enum RolesPermission {
-  CREATE_ROLE = 'role_create',
-  DELETE_ROLE = 'role_delete',
-}
-
-export default RolesPermission;
--- user-svc/src/auth/permissions/users-permission.enum.ts
+++
@@ -1,6 +0,0 @@
-enum UsersPermission {
-  CREATE_USER = 'user_create',
-  DELETE_USER = 'user_delete',
-}
-
-export default UsersPermission;
